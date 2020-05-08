6ace0141b0699774b7f7a39bb8076c6b09475117
franekruzicevic <62648556+franekruzicevic@users.noreply.github.com>
2020-05-08 22:03:17 +0200

    Add files via upload

---
+++ Game/game.py
@@ -0,0 +1,233 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500,480))
+
+pygame.display.set_caption('First Game')
+#glavnilik(bil)
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg') #pozadina
+char = pygame.image.load('standing.png')
+
+clock = pygame.time.Clock()
+#muzika u pozadini
+music = pygame.mixer.music.load('music.mp3')
+pygame.mixer.music.play(-1)
+
+score = 0
+#glavnilik(bil)atributi
+class player(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.jumpCount = 10
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+        self.standing = True
+        self.hitbox = (self.x + 17, self.y + 11, 29, 52)
+    #crtanjelika    
+    def draw(self,win):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if not (self.standing):
+            if self.left:
+                win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+                self.walkCount += 1
+            elif self.right:
+                win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+                self.walkCount += 1
+        else:
+            if self.right:
+                win.blit(walkRight[0], (self.x, self.y))
+            else:
+                win.blit(walkLeft[0], (self.x, self.y))
+        self.hitbox = (self.x + 17, self.y + 11, 29, 52)
+        #pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
+
+    def hit(self):
+        self.isJump = False
+        self.jumpCount = 10
+        self.x = 60
+        self.y =410
+        self.walkCount = 0
+        font1 = pygame.font.SysFont('comicsans', 100)
+        text = font1.render('-5', 1, (255,0,0))
+        win.blit(text, (250 - (text.get_width()/2),200))
+        pygame.display.update()
+        i = 0
+        while i < 200:
+            pygame.time.delay(10)
+            i += 1
+            for enent in pygame.event.get():
+                if enent.type == pygame.QUIT:
+                    i = 301
+                    pygame.quit()
+            
+#metakatributi
+class projectile(object):
+    def __init__(self, x, y, radius, color, facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 8 * facing
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+#suparnik(nick)atributi
+class enemy(object):
+    walkRight = [pygame.image.load('R1E.png'), pygame.image.load('R2E.png'), pygame.image.load('R3E.png'), pygame.image.load('R4E.png'), pygame.image.load('R5E.png'), pygame.image.load('R6E.png'), pygame.image.load('R7E.png'), pygame.image.load('R8E.png'), pygame.image.load('R9E.png'), pygame.image.load('R10E.png'), pygame.image.load('R11E.png')]
+    walkLeft = [pygame.image.load('L1E.png'), pygame.image.load('L2E.png'), pygame.image.load('L3E.png'), pygame.image.load('L4E.png'), pygame.image.load('L5E.png'), pygame.image.load('L6E.png'), pygame.image.load('L7E.png'), pygame.image.load('L8E.png'), pygame.image.load('L9E.png'), pygame.image.load('L10E.png'), pygame.image.load('L11E.png')]
+
+    def __init__(self, x, y, width, height, end):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.end = end
+        self.path = [self.x, self.end]
+        self.walkCount = 0
+        self.vel = 3
+        self.hitbox = (self.x + 17, self.y + 2, 31, 57)
+        self.health = 10
+        self.visible = True
+
+    def draw(self,win):
+        self.move()
+        if self.visible:
+            if self.walkCount + 1 >= 33:
+                self.walkCount = 0
+            if self.vel > 0:
+                win.blit(self.walkRight[self.walkCount //3], (self.x, self.y))
+                self.walkCount += 1
+            else:
+                win.blit(self.walkLeft[self.walkCount //3], (self.x, self.y))
+                self.walkCount += 1
+
+            pygame.draw.rect(win, (255,0,0), (self.hitbox[0], self.hitbox[1] - 20, 50, 10))
+            pygame.draw.rect(win, (0,125,0), (self.hitbox[0], self.hitbox[1] - 20, 50 - (5 * (10 - self.health)), 10))
+            self.hitbox = (self.x + 17, self.y + 2, 31, 57)
+            #pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
+
+    def move(self): #pomicanje
+        if self.vel > 0:
+            if self.x + self.vel < self.path[1]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walkCount = 0
+        else:
+            if self.x - self.vel > self.path[0]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walkCount = 0
+    def hit(self): #udarac
+        if self.health > 0:
+            self.health -=1
+        else:
+            self.visible = False
+        print('hit')
+#crtanje
+def redrawGameWindow():
+    win.blit(bg, (0,0))
+    text = font.render('Score: ' + str(score), 1, (255,0,255))
+    win.blit(text, (350,10))
+    bil.draw(win)
+    nick.draw(win)
+    for bullet in bullets:
+        bullet.draw(win)
+    pygame.display.update()
+    
+#mainloop
+font = pygame.font.SysFont('comicsans', 30, True, True)
+bil = player(200, 410, 64, 64)
+nick = enemy(100, 410, 64, 64, 450)
+shootLoop = 0
+bullets = []
+run = True
+while run:
+    clock.tick(27)
+
+    if nick.visible == True:
+        if bil.hitbox[1] < nick.hitbox[1] + nick.hitbox[3] and bil.hitbox[1] + bil.hitbox[3] > nick.hitbox[1]:
+            if bil.hitbox[0] + bil.hitbox[2] > nick.hitbox[0] and bil.hitbox[0] < nick.hitbox[0] + nick.hitbox[2]:
+                bil.hit()
+                score -= 5
+
+    if shootLoop > 0:
+        shootLoop += 1
+    if shootLoop > 3:
+        shootLoop = 0
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    for bullet in bullets:
+        if nick.visible == True:
+            if bullet.y - bullet.radius < nick.hitbox[1] + nick.hitbox[3] and bullet.y + bullet.radius > nick.hitbox[1]:
+                if bullet.x + bullet.radius > nick.hitbox[0] and bullet.x - bullet.radius < nick.hitbox[0] + nick.hitbox[2]:
+                    nick.hit()
+                    score += 1
+                    bullets.pop(bullets.index(bullet))
+                
+        if bullet.x < 500 and bullet.x > 0:
+            bullet.x += bullet.vel
+        else:
+            bullets.pop(bullets.index(bullet))
+
+    keys = pygame.key.get_pressed()
+#određivanjeTipki
+    if keys[pygame.K_SPACE] and shootLoop == 0:
+        if bil.left:
+            facing = -1
+        else:
+            facing = 1
+        if len(bullets) < 5:
+            bullets.append(projectile(round(bil.x + bil.width // 2), round(bil.y + bil.height // 2), 6, (0, 0, 0), facing))
+            
+        shootLoop = 1
+
+    if keys[pygame.K_LEFT] and bil.x > bil.vel:
+        bil.x -= bil.vel
+        bil.left = True
+        bil.right = False
+        bil.standing = False
+    elif keys[pygame.K_RIGHT] and bil.x < 500 - bil.width - bil.vel:
+        bil.x += bil.vel
+        bil.right = True
+        bil.left = False
+        bil.standing = False
+    else:
+        bil.standing = True
+        bil.walkCount = 0
+    if not (bil.isJump):
+        if keys[pygame.K_UP]:
+            bil.isJump = True
+            bil.right = False
+            bil.left = False
+            bil.walkCount = 0
+    else:
+        if bil.jumpCount >= -10:
+            neg = 1
+            if bil.jumpCount < 0:
+                neg = -1
+            bil.y -= (bil.jumpCount ** 2) * 0.5 * neg
+            bil.jumpCount -= 1
+        else:
+            bil.isJump = False
+            bil.jumpCount = 10
+            
+    redrawGameWindow()
+
+
+pygame.quit()
